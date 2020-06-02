087e8c532514a01d0380bbc436cdc57b93d68275
gff1055 <guilhermeferreirafb@gmail.com>
2020-06-02 04:23:02 -0300

    estilos css

--- projeto-sigemep/resources/views/user/login.blade.php
+++ projeto-sigemep/resources/views/user/login.blade.php
@@ -4,33 +4,42 @@
 		<link rel="stylesheet" href="{{asset('css/stylesheet.css')}}">
 		<title></title>
 	</head>
-	<body id="bodyPaginaInicial">
-		<section id="conteudo-view" class="login">
-			{!!Form::open([
-				'route'=>'user.login',
-				'method'=>'post',
-				'class' => 'formLogin'
-			]) !!}
-				Entrar
-				<br>
-				<br>
+	<body class="bodyPaginaLogin">
+		<section id="conteudo-view">
+			<div class="divFormLogin">
+				{!!Form::open([
+					'route'=>'user.login',
+					'method'=>'post',
+					'class' => 'formLogin'
+				]) !!}
+					Entrar
+					<br>
+					<br>	
 
-				{!! Form::text('username', null, [
-					'class' => 'atrForm',
-					'placeholder' => "Usuario"
-				]) !!}
-				<br>
-				{!! Form::password('password', [
-					'class' => 'atrForm',
-					'placeholder' => 'Senha'
-				]) !!}
-				<br>
-				{!! Form::submit('Entrar', [
-					'class' => 'atrForm',
-				]) !!}
+					{!! Form::text('username', null, [
+						'class' => 'atrForm',
+						'placeholder' => "Usuario"
+					]) !!}
+
+					<br>
+
+					{!! Form::password('password', [
+						'class' => 'atrForm',
+						'placeholder' => 'Senha'
+					]) !!}
+
+					<br>
+
+					<div class="divBtEnviar">
+						{!! Form::submit('Entrar', [
+							'class' => 'atrForm',
+						]) !!}
+					</div>
+
+					{!! Form::close() !!}
+
 				{!! Form::close() !!}
-
-			{!! Form::close() !!}
+			</div>
 		</section>
 	</body>
 </html>
