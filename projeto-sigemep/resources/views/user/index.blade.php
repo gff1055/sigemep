4c055217a30ae79298328c2f0921fc0b1d9d461c
gff1055 <guilhermeferreirafb@gmail.com>
2020-12-07 15:23:16 -0300

    Pagina do usuario e menu sanduiche

--- projeto-sigemep/resources/views/user/index.blade.php
+++ projeto-sigemep/resources/views/user/index.blade.php
@@ -1,8 +1,39 @@
 @extends('templates.basic')
 
 @section('content')
-dd
+
+<section class="topMenuBar">
 	
+	<div class="itemTopMenuBar">
+		<div id="menuToggle">
+			<input type="checkbox" />
+			<span></span>
+			<span></span>
+			<span></span>
+			<ul id="menu">
+				<a href="#"><li>Home</li></a>
+				<a href="#"><li>About</li></a>
+				<a href="#"><li>Info</li></a>
+				<a href="#"><li>Contact</li></a>
+				<a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+			</ul>
+		</div>
+	</div>
+		<!--<div class="subMenuSandwich">
+			<ul>
+				<li> Opcao1 </li>
+				<li> Opcao1 </li>
+				<li> Opcao1 </li>
+			</ul>
+		</div>-->
+	
+	<div class="itemTopMenuBar"><span class="logo">KlinicK</span></div>
+	<div class="itemTopMenuBar"><a href="#">Seguranca</a></div>
+	<div class="itemTopMenuBar"><a href="#">Ajuda</a></div>
+	<div class="itemTopMenuBar"><a href="#">Contato</a></div>
+</section>
+
+
 
 	
 @endsection
